49582c4651dc5f33e95d5876f0dbbe0f525cfa30
Francisco Camino <francisco.camino@solera.com>
2023-07-10 17:04:12 +0200

    falta arreglar el cors

--- Client/appbank_project/src/components/Input.jsx
+++ Client/appbank_project/src/components/Input.jsx
@@ -1,10 +1,15 @@
+import PropTypes from 'prop-types';
 import Input from '@mui/joy/Input';
 
 function InputComponent({placeholder}) {
   return (
-    <div class= "InputContainer">
+    <div className= "InputContainer">
   <Input placeholder={placeholder}  variant="outlined"  sx={{ borderRadius:4}} />
 </div>)
 }
+
+InputComponent.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+};
 
 export default InputComponent;
--- Client/appbank_project/src/components/Singup.jsx
+++ Client/appbank_project/src/components/Singup.jsx
@@ -1,111 +1,111 @@
-import React from "react";
 import "../App.css";
+import { useState } from "react";
 import InputComponent from "./Input";
 import ButtonComponent from "./Button";
 import H2 from "./H2";
 import Logo from "./Logo";
+import axios from "axios";
 
 function Singup() {
   const [formData, setFormData] = useState({
+    firstName: "",
 
-    firstName: '',
+    secondName: "",
 
-    secondName: '',
+    username: "",
 
-    username: '',
+    password: "",
 
-    password: '',
-
-    confirmPassword: ''
-
+    confirmPassword: "",
   });
 
   const handleSubmit = (event) => {
-
     event.preventDefault();
 
-
-
-
-    axios.post('http://localhost:9500/user', formData)
+    axios
+      .post("http://localhost:9500/user", formData)
 
       .then((response) => {
-
-        console.log(response.data);  // Respuesta del servidor
+        console.log(response.data); // Respuesta del servidor
 
         // Puedes realizar acciones adicionales después de agregar el usuario
-
       })
 
       .catch((error) => {
-
         console.error(error);
 
         // Manejar errores de manera adecuada
-
       });
-
   };
 
   return (
     <>
-    <div class="signupFrm"></div>
-    <form className="form-container">
-      <Logo />
-      <H2 />
-      <InputComponent  type="text"
-
+      <form className="form-container" onSubmit={handleSubmit}>
+        <Logo />
+        <H2 />
+        <InputComponent
+          type="text"
           name="First Name"
-
           value={formData.firstName}
+          onChange={(event) =>
+            setFormData({ ...formData, name: event.target.value })
+          }
+          placeholder="First Name*"
+        ></InputComponent>
 
-          onChange={(event) => setFormData({ ...formData, name: event.target.value })} placeholder="First Name*"></InputComponent>
-
-
-
-      <InputComponent></InputComponent>
-      <InputComponent  type="text"
-
+        
+        <InputComponent
+          type="text"
           name="Last Name"
-
           value={formData.secondName}
+          onChange={(event) =>
+            setFormData({ ...formData, name: event.target.value })
+          }
+          placeholder="Last Name*"
+        ></InputComponent>
 
-          onChange={(event) => setFormData({ ...formData, name: event.target.value })} placeholder="Last Name*"></InputComponent>
+        
 
-      <InputComponent></InputComponent>
+        <InputComponent
+          type="text"
+          name="Username"
+          value={formData.username}
+          onChange={(event) =>
+            setFormData({ ...formData, name: event.target.value })
+          }
+          placeholder="Username"
+        ></InputComponent>
 
-      <InputComponent  type="text"
-
-name="Username"
-
-value={formData.username}
-
-onChange={(event) => setFormData({ ...formData, name: event.target.value })} placeholder="Username"></InputComponent>
-
-<InputComponent></InputComponent>
-      <InputComponent  type="text"
-
+        
+        <InputComponent
+          type="text"
           name="Password"
-
           value={formData.password}
+          onChange={(event) =>
+            setFormData({ ...formData, name: event.target.value })
+          }
+          placeholder="Password*"
+        ></InputComponent>
 
-          onChange={(event) => setFormData({ ...formData, name: event.target.value })} placeholder="Password*"></InputComponent>
+        
 
-      <InputComponent></InputComponent>
-      
-      <InputComponent  type="text"
+        <InputComponent
+          type="text"
+          name="Confirm Password"
+          value={formData.confirmPassword}
+          onChange={(event) =>
+            setFormData({ ...formData, name: event.target.value })
+          }
+          placeholder="Confirm Password*"
+        ></InputComponent>
 
-name="Confirm Password"
-
-value={formData.confirmPassword}
-
-onChange={(event) => setFormData({ ...formData, name: event.target.value })} placeholder="Confirm Password*"></InputComponent>
-
-<InputComponent></InputComponent>
-      <ButtonComponent>SING UP</ButtonComponent>
-    </form>
+        
+        <ButtonComponent type="submit">SING UP</ButtonComponent>
+      </form>
     </>
   );
+  
 }
+
 
 export default Singup;

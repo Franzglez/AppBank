43ac1e27a22c18e091715c46e824e1899d09d0e7
Francisco Camino <francisco.camino@solera.com>
2023-07-13 15:51:18 +0200

    componente UserSetting

--- Client/appbank_project/src/components/UserSetting.jsx
+++ Client/appbank_project/src/components/UserSetting.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { TextField, Button } from '@mui/material';
 
-function UserSetting() {
+const Formulario = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para manejar los datos del formulario
+  };
+
   return (
-    <div>UserSetting</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <TextField label="Campo 1" variant="outlined" fullWidth margin="normal" />
+      <TextField label="Campo 2" variant="outlined" fullWidth margin="normal" />
+      <TextField label="Campo 3" variant="outlined" fullWidth margin="normal" />
+      <TextField label="Campo 4" variant="outlined" fullWidth margin="normal" />
+      <Button type="submit" variant="contained" color="primary">
+        Enviar
+      </Button>
+    </form>
+  );
+};
 
-export default UserSetting
+export default Formulario;
